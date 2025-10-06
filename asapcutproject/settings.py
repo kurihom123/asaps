2caabacc8412dc2356b596ad668f7a3030a9ddfa
kuriho matiko <you@example.com>
2025-10-06 15:05:42 +0300

    static files commit

--- asapcutproject/settings.py
+++ asapcutproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zv^a2puy$h&qxbtungh-kq-%g&9grp+f-l_(_=xyzpvxj09cl4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['asaps-4.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -124,20 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# Base url to serve media files
-MEDIA_URL = '/media/'
-
-# Path where media is stored
-MEDIA_ROOT = BASE_DIR / 'media/'
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
