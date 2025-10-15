86a321ee2112e9c8c26e6f0ea83f42eb62ad5a2e
kuriho matiko <you@example.com>
2025-10-15 13:54:13 +0300

    index2 changed

--- asapcutapp/views/base_view.py
+++ asapcutapp/views/base_view.py
@@ -3,6 +3,12 @@ from ..models.organization_model import Association
 from ..models.user_model import UserProfile
 
 def base(request):
-    associations = Association.objects.all()
-    leaders = UserProfile.objects.select_related('level', 'association', 'position').all()
-    return render(request, 'index.html', {'associations': associations, 'leaders': leaders})
+     context = {
+        'president': UserProfile.objects.filter(position__name__icontains='President').first(),
+        'vice_president': UserProfile.objects.filter(position__name__icontains='Vice President').first(),
+        'general_secretary': UserProfile.objects.filter(position__name__icontains='General Secretary').first(),
+        'treasurer': UserProfile.objects.filter(position__name__icontains='Treasurer').first(),
+        'deputy_secretary': UserProfile.objects.filter(position__name__icontains='Deputy Secretary').first(),
+        'associations': Association.objects.all(),
+    }
+    return render(request, 'index.html', context)
