cadb795b846c939b1ba8df16ef7d8a809d0f2641
kuriho matiko <you@example.com>
2025-10-20 15:30:02 +0300

    new updated

--- asapcutapp/views/dashboard.py
+++ asapcutapp/views/dashboard.py
@@ -8,7 +8,7 @@ from asapcutapp.models import University, Association, Contribution
 def dashboard(request):
     total_universities = University.objects.count()
     total_associations = Association.objects.count()
-    total_contributions = Contribution.objects.count()
+    total_contributions = Contribution.objects.filter(amount_paid__gt=0).count() 
     total_members = Association.objects.aggregate(total_members=Sum('member_number'))['total_members'] or 0
 
     context = {
